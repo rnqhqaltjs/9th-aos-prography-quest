029df92617a278107828942e1da6f77ff44e2670
김승우 <rnqhqaltjs@naver.com>
2024-01-30 13:44:14 +0900

    [refactor] Hilt 의존성 주입

---
+++ app/src/main/java/com/prography/quest/ui/bookmark/BookmarkViewModel.kt
@@ -0,0 +1,30 @@
+package com.prography.quest.ui.bookmark
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.prography.quest.data.model.PhotosResponseItem
+import com.prography.quest.data.repository.PhotosRepository
+import com.prography.quest.data.repository.RandomPhotoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BookmarkViewModel @Inject constructor(
+    private val randomPhotoRepository: RandomPhotoRepository
+): ViewModel() {
+
+    private val _getRandomResult = MutableLiveData<List<PhotosResponseItem>>()
+    val getRandomResult: LiveData<List<PhotosResponseItem>> get() = _getRandomResult
+
+    fun getRandomPhoto() = viewModelScope.launch {
+        val response = randomPhotoRepository.getRandomPhoto(1)
+        if (response.isSuccessful) {
+            response.body()?.let { body ->
+                _getRandomResult.postValue(body)
+            }
+        }
+    }
+}
