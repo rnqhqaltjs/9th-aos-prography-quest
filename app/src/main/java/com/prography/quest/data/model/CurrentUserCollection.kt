663247b8fa00325a9e2ae3655972b6b6aa5d8956
김승우 <rnqhqaltjs@naver.com>
2024-01-30 13:38:51 +0900

    [feat] 레트로핏 통신 모델 추가

---
+++ app/src/main/java/com/prography/quest/data/model/CurrentUserCollection.kt
@@ -0,0 +1,23 @@
+package com.prography.quest.data.model
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class CurrentUserCollection(
+    @field:Json(name = "cover_photo")
+    val coverPhoto: Any,
+    @field:Json(name = "id")
+    val id: Int,
+    @field:Json(name = "last_collected_at")
+    val lastCollectedAt: String,
+    @field:Json(name = "published_at")
+    val publishedAt: String,
+    @field:Json(name = "title")
+    val title: String,
+    @field:Json(name = "updated_at")
+    val updatedAt: String,
+    @field:Json(name = "user")
+    val user: Any
+)
