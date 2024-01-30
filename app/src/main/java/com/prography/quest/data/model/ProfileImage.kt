83a5ba85dd5e1530ae608acbdbb47f8f8cefd043
김승우 <rnqhqaltjs@naver.com>
2024-01-30 13:37:09 +0900

    [feat] 통신 모델 추가

---
+++ app/src/main/java/com/prography/quest/data/model/ProfileImage.kt
@@ -0,0 +1,15 @@
+package com.prography.quest.data.model
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class ProfileImage(
+    @field:Json(name = "large")
+    val large: String,
+    @field:Json(name = "medium")
+    val medium: String,
+    @field:Json(name = "small")
+    val small: String
+)
