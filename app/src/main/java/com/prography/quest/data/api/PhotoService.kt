540165fc81ab67aee29d0fce14d1d566ac5a7a0b
김승우 <rnqhqaltjs@naver.com>
2024-02-02 18:40:34 +0900

    [feat] 인터페이스 정의

---
+++ app/src/main/java/com/prography/quest/data/api/PhotoService.kt
@@ -0,0 +1,29 @@
+package com.prography.quest.data.api
+
+import com.prography.quest.data.model.photosresponse.PhotosResponseItem
+import com.prography.quest.util.Constants.CLIENT_ID
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface PhotoService {
+    @Headers("Accept-Version: v1", "Authorization: Client-ID $CLIENT_ID")
+    @GET("photos")
+    suspend fun getPhotos(
+        @Query("page") page: Int,
+        @Query("per_page") per_page: Int,
+        @Query("order_by") order_by: String
+    ): Response<List<PhotosResponseItem>>
+
+    @Headers("Accept-Version: v1", "Authorization: Client-ID $CLIENT_ID")
+    @GET("photos/{id}")
+    suspend fun getPhotoDetails(@Path("id") id: String): Response<PhotosResponseItem>
+
+    @Headers("Accept-Version: v1", "Authorization: Client-ID $CLIENT_ID")
+    @GET("photos/random")
+    suspend fun getRandomPhoto(
+        @Query("count") count: Int
+    ): Response<List<PhotosResponseItem>>
+}
