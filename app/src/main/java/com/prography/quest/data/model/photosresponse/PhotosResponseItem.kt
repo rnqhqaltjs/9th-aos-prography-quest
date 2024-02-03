73d8b013c0146fa816bad829090c2bdcdf243d71
김승우 <rnqhqaltjs@naver.com>
2024-02-03 14:19:33 +0900

    [feat] 통신 모델 구현

--- app/src/main/java/com/prography/quest/data/model/photosresponse/PhotosResponseItem.kt
+++ app/src/main/java/com/prography/quest/data/model/photosresponse/PhotosResponseItem.kt
@@ -1,23 +1,19 @@
 package com.prography.quest.data.model.photosresponse
 
 
-import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import kotlinx.parcelize.Parcelize
-import kotlinx.parcelize.RawValue
 
-@Parcelize
 @JsonClass(generateAdapter = true)
 data class PhotosResponseItem(
     @field:Json(name = "blur_hash")
-    val blurHash: String,
+    val blurHash: String?,
     @field:Json(name = "color")
     val color: String,
     @field:Json(name = "created_at")
     val createdAt: String,
     @field:Json(name = "current_user_collections")
-    val currentUserCollections: @RawValue List<CurrentUserCollection>,
+    val currentUserCollections: List<CurrentUserCollection>,
     @field:Json(name = "description")
     val description: String?,
     @field:Json(name = "height")
@@ -29,13 +25,13 @@ data class PhotosResponseItem(
     @field:Json(name = "likes")
     val likes: Int,
     @field:Json(name = "links")
-    val links: @RawValue Links,
+    val links: Links,
     @field:Json(name = "updated_at")
     val updatedAt: String,
     @field:Json(name = "urls")
-    val urls: @RawValue Urls,
+    val urls: Urls,
     @field:Json(name = "user")
-    val user: @RawValue User,
+    val user: User,
     @field:Json(name = "width")
     val width: Int
-) : Parcelable
+)
